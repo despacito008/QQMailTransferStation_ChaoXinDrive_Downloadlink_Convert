6e2b34ce44adf7ce6f8c7cb8c66a22d46eca4e72
axutongxue <axutongxue@qq.com>
2019-05-04 19:13:40 +0800

    第一次提交

---
+++ QQMaiLlinkConvert/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace QQMaiLlinkConvert
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.convert = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TopcheckBox = new System.Windows.Forms.CheckBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // convert
+            // 
+            this.convert.BackColor = System.Drawing.Color.Transparent;
+            this.convert.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.convert.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.convert.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.convert.Location = new System.Drawing.Point(209, 96);
+            this.convert.Name = "convert";
+            this.convert.Size = new System.Drawing.Size(256, 39);
+            this.convert.TabIndex = 0;
+            this.convert.Text = "转换链接并复制到剪贴板(&C)";
+            this.convert.UseVisualStyleBackColor = false;
+            this.convert.Click += new System.EventHandler(this.convert_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(484, 46);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "1.复制QQ邮箱中转站下载链接（超星网盘网页源代码文件class）后，点击下方按钮即可获取分享链接";
+            // 
+            // TopcheckBox
+            // 
+            this.TopcheckBox.AutoSize = true;
+            this.TopcheckBox.Checked = true;
+            this.TopcheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.TopcheckBox.Location = new System.Drawing.Point(35, 105);
+            this.TopcheckBox.Name = "TopcheckBox";
+            this.TopcheckBox.Size = new System.Drawing.Size(142, 22);
+            this.TopcheckBox.TabIndex = 2;
+            this.TopcheckBox.Text = "窗口是否置顶";
+            this.TopcheckBox.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(485, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "2.选中超星网盘文件后，快捷键:Ctrl+Q，也可获取分享链接";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(508, 154);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TopcheckBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.convert);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(530, 210);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(530, 210);
+            this.Name = "Form1";
+            this.Text = "获取QQ邮箱中转站 / 超星云盘分享链接";
+            this.Deactivate += new System.EventHandler(this.Form1_Deactivate);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button convert;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox TopcheckBox;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ QQMaiLlinkConvert/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace QQMaiLlinkConvert
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Deactivate(object sender, EventArgs e)
+        {
+            if (TopcheckBox.Checked == true)
+            {
+                this.TopMost = true;
+            }
+            else
+            {
+                this.TopMost = false;
+            }
+        }
+        private void convert_Click(object sender, EventArgs e)
+        {
+
+            string Share1 = "https://pan-yz.chaoxing.com/external/m/file/";
+            string Share2 = "http://mail.qq.com/cgi-bin/ftnExs_download?k=";
+            string Share3 = "&t=exs_ftn_download&code=";
+            string originallink = "null";
+            string sharelink;
+
+            //获取剪切版数据
+            IDataObject data = Clipboard.GetDataObject();
+            if (data.GetDataPresent(DataFormats.Text))
+                originallink = data.GetData(DataFormats.Text).ToString();
+
+            if (originallink.Contains("fileName"))
+            {
+                string[] chaoxin = originallink.Split(new char[1] { '_' });
+                sharelink = Share1 + chaoxin[2];
+                Clipboard.SetDataObject(sharelink);
+            }
+            if (originallink.Contains("https://mail.qq.com/"))
+            {
+                string[] QQmail = originallink.Split(new string[] { "code=", "&k=" }, StringSplitOptions.RemoveEmptyEntries);
+                sharelink = Share2 + QQmail[2] + Share3 + QQmail[1];
+                Clipboard.SetDataObject(sharelink);
+            }
+            MessageBox.Show("链接转换成功并已复制到剪贴板", "复制成功");
+        }
+
+        //模拟键盘事件
+        [DllImport("user32.dll", EntryPoint = "keybd_event", SetLastError = true)]
+        public static extern void keybd_event(Keys bVk, byte bScan, uint dwFlags, uint dwExtraInfo);
+        public const int KEYEVENTF_KEYUP = 2;
+
+        //模拟鼠标事件
+        [System.Runtime.InteropServices.DllImport("user32")]
+        private static extern int mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+        //移动鼠标 
+        const int MOUSEEVENTF_MOVE = 0x0001;
+        //模拟鼠标左键按下 
+        const int MOUSEEVENTF_LEFTDOWN = 0x0002;
+        //模拟鼠标左键抬起 
+        const int MOUSEEVENTF_LEFTUP = 0x0004;
+        //模拟鼠标右键按下 
+        const int MOUSEEVENTF_RIGHTDOWN = 0x0008;
+        //模拟鼠标右键抬起 
+        const int MOUSEEVENTF_RIGHTUP = 0x0010;
+        //模拟鼠标中键按下 
+        const int MOUSEEVENTF_MIDDLEDOWN = 0x0020;
+        //模拟鼠标中键抬起 
+        const int MOUSEEVENTF_MIDDLEUP = 0x0040;
+        //标示是否采用绝对坐标 
+        const int MOUSEEVENTF_ABSOLUTE = 0x8000;
+        
+        //导入dll文件
+        [DllImport("user32.dll")]
+        //函数声明
+        public static extern int GetAsyncKeyState(int vKey);
+        //时钟监控事件 Interval =100
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //需要监控什么按键就写某个按键的ASCII码
+            if (GetAsyncKeyState(17) != 0 & GetAsyncKeyState(81) != 0)
+            {
+                //模拟鼠标右键
+                mouse_event(MOUSEEVENTF_RIGHTDOWN | MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0);
+                System.Threading.Thread.Sleep(200);
+                //模拟Ctrl+Shift+I
+                keybd_event(Keys.N, 0, 0, 0);
+                keybd_event(Keys.N, 0, KEYEVENTF_KEYUP, 0);
+                //延时1S
+                System.Threading.Thread.Sleep(1500);
+                //模拟F2
+                keybd_event(Keys.F2, 0, 0, 0);
+                keybd_event(Keys.F2, 0, KEYEVENTF_KEYUP, 0);
+                System.Threading.Thread.Sleep(300);
+                //模拟Ctrl+A
+                keybd_event(Keys.ControlKey, 0, 0, 0);
+                keybd_event(Keys.A, 0, 0, 0);
+                keybd_event(Keys.ControlKey, 0, KEYEVENTF_KEYUP, 0);
+                keybd_event(Keys.A, 0, KEYEVENTF_KEYUP, 0);
+                System.Threading.Thread.Sleep(300);
+                //模拟Ctrl+C
+                keybd_event(Keys.ControlKey, 0, 0, 0);
+                keybd_event(Keys.C, 0, 0, 0);
+                keybd_event(Keys.ControlKey, 0, KEYEVENTF_KEYUP, 0);
+                keybd_event(Keys.C, 0, KEYEVENTF_KEYUP, 0);
+                System.Threading.Thread.Sleep(800);
+
+                string Share1 = "https://pan-yz.chaoxing.com/external/m/file/";
+                string originallink = "null";
+                string sharelink;
+
+                IDataObject data = Clipboard.GetDataObject();
+                if (data.GetDataPresent(DataFormats.Text))
+                {
+                    //如果剪贴板中的数据是文本格式 
+                    originallink = (string)data.GetData(DataFormats.Text);
+                    string[] chaoxin = originallink.Split(new char[2] { '_', '"' });
+                    sharelink = Share1 + chaoxin[7];
+                    Clipboard.SetDataObject(sharelink);
+                    notifyIcon1.ShowBalloonTip(200, "链接转换成功并已复制到剪贴板", sharelink, ToolTipIcon.None);
+                    notifyIcon1.Dispose();
+                }
+            }                   
+            }
+        }
+    }
+
